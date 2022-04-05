c3bf4b53cc00c78a364c6de8e6a71c645885c8f6
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2022-04-05 19:04:49 +0400

    kidsDrawingApp

--- kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/DrowingView.kt
+++ kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/DrowingView.kt
@@ -17,6 +17,7 @@ class DrowingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     private var mBrushSize: Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas: Canvas? = null
+    private var mPath = ArrayList<CustomPath>()
 
     init {
         setUpDrawing()
@@ -44,6 +45,13 @@ class DrowingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap( mCanvasBitmap!!, 0f,0f, mCanvasPaint)
+
+        for (path in mPath){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = path.color
+            canvas.drawPath(path, mDrawPaint!!)
+
+        }
         if (!mDrawPath!!.isEmpty){
             mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPath!!.color
@@ -76,6 +84,7 @@ class DrowingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 }
             }
             MotionEvent.ACTION_UP ->{
+                mPath.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return  false
