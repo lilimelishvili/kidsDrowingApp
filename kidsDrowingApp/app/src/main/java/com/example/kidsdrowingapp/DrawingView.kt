276ba19eca959c36eed48dbc3098966cc0f89102
lilimelishvili <91181560+lilimelishvili@users.noreply.github.com>
2022-04-07 22:31:35 +0400

    kidsDrawingApp

--- kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/DrawingView.kt
+++ kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/DrawingView.kt
@@ -101,6 +101,13 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) 
                 newSize, resources.displayMetrics)
         mDrawPaint!!.strokeWidth = mBrushSize
     }
+
+    fun setColor(newColor: String){
+        color = Color.parseColor(newColor)
+    }
+
+
+
     internal inner class CustomPath (var color: Int, var brushThickness: Float) : Path() {
 
     }
--- kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/MainActivity.kt
+++ kidsDrowingApp/app/src/main/java/com/example/kidsdrowingapp/MainActivity.kt
@@ -4,17 +4,33 @@ import android.app.Dialog
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 
 class MainActivity : AppCompatActivity() {
 
 
     private var  drawingView : DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         drawingView = findViewById(R.id.drawing_view)
         drawingView?.setSizeForBrush(20.toFloat())
+
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_paint_colors)
+
+        mImageButtonCurrentPaint = linearLayoutPaintColors[1] as ImageButton
+        mImageButtonCurrentPaint!!.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+        )
 
 
         val ib_brush : ImageButton = findViewById(R.id.ib_brush)
@@ -48,6 +64,25 @@ class MainActivity : AppCompatActivity() {
 
 
         brushDialog.show()
-
     }
+    fun paintClicked(view: View){
+      //  Toast.makeText(this, "clicked paint",  Toast.LENGTH_LONG).show()
+  if (view !== mImageButtonCurrentPaint){
+      val imageButton = view as ImageButton
+      val colorTag = imageButton.tag.toString()
+      drawingView?.setColor(colorTag)
+
+
+      imageButton.setImageDrawable(
+          ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
+              )
+      mImageButtonCurrentPaint?.setImageDrawable(
+          ContextCompat.getDrawable(this, R.drawable.pallet_normal)
+      )
+      mImageButtonCurrentPaint = view
+
+
+  }
+    }
+
 }
